edfc937ab64e745c0202d32f45fdb8effe1e2db4
DS-jr <93123740+DS-jr@users.noreply.github.com>
2022-01-09 13:03:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,6 @@ df2 = pd.read_csv("signatures.tsv", sep="\t")
 
 @app5.get("/")
 async def root(patient_id): 
-    return df2[df2.iloc[:, 0].str.contains(patient_id)].to_json(orient="index")     # Add a ?JSON-fy function  ***Check FastAPI or Pandas, so the output is strictly in JSON format
+    return df2[df2.iloc[:, 0].str.contains(patient_id)].to_json(orient="index")   # Input examples: NSCLC1155  NSCLC825 
 
-
-#input_name = input("Enter the patient's name (from index column in 'signatures.tsv' file): ") # Input example: NSCLC1155  NSCLC825 
-#	return {"message2": "You are always in the optimal place with the optimal LESSONS selected specifically for your max self-dev"} 
-
-
-
+#	return {"message2": "You are always in the optimal place with the optimal lessons selected specifically for your max self-dev"} 
